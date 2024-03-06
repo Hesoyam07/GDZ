4b75d1c5a148476c440c713772a6e415e57c9504
Dmitriy <140389449+Hesoyam07@users.noreply.github.com>
2024-03-07 00:07:50 +0300

    removed unusual code

--- GDZ/ViewController.swift
+++ GDZ/ViewController.swift
@@ -9,18 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let testButton: UIButton = .makeButton(text: "test", backgroundColor: .buyButtonColor, stringColor: .viewBackgroundColor)
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.viewBackgroundColor
-        view.addSubview(testButton)
-        testButton.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            testButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            testButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            testButton.widthAnchor.constraint(equalToConstant: 200),
-            testButton.heightAnchor.constraint(equalToConstant: 100)
-        ])
+        
     }
 
     
