c9ef958ddb4d7a55a84d7ac0678fab8b31c84d11
Dmitriy <140389449+Hesoyam07@users.noreply.github.com>
2024-03-13 23:50:59 +0300

    Revert "Merge current changes into grade branch "

--- GDZ/SceneDelegate.swift
+++ GDZ/SceneDelegate.swift
@@ -16,8 +16,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         
-        let viewController = TabBarContoller()
-            window.rootViewController = viewController
+        let viewController = ViewController()
+            let navigationController = UINavigationController(rootViewController: viewController)
+            window.rootViewController = navigationController
             window.makeKeyAndVisible()
             self.window = window
     }
